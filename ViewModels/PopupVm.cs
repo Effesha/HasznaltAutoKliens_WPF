7c2ba24e86818b0c77d1009aba19edebf3837d74
Antus Balázs <bantus96@hotmail.com>
2025-11-15 15:44:36 +0100

    submodule dirty fix try

--- ViewModels/PopupVm.cs
+++ ViewModels/PopupVm.cs
@@ -7,7 +7,6 @@ namespace HasznaltAuto.Desktop.ViewModels
     public class PopupVm
     {
         public CarDto CarDto { get; set; } = new();
-
         public List<MakeType> Makes { get; set; } = [];
         public List<ModelType> Models { get; set; } = [];
         public List<FuelType> FuelTypes { get; set; } = [];
